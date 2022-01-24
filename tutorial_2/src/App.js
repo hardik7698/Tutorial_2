7d9fc92742836e4c2559fbac1b333551cb0593a6
mesvania <mesvania@dal.ca>
2022-01-24 11:16:22 -0400

    code added

---
+++ tutorial_2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to my first Individual code.I have created a sample code for tutorial 2!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
